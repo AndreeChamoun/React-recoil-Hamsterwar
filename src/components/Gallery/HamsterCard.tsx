8d64e86dfa77a75faf816de9e6825436320b7d58
André C <sabahchamoun@hotmail.com>
2022-09-08 14:16:57 +0200

    make it better

--- src/components/Gallery/HamsterCard.tsx
+++ src/components/Gallery/HamsterCard.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { useState } from "react";
+import { useRecoilState } from 'recoil';
 import styles from "../../styles/hamstercard.module.css"
 import { HamsterModel } from "../../models/HamsterModel";
+import allHamsters from "../../atoms/allHamsters";
+import { makeImg } from "../../utils";
 
 interface Props {
   hamster: HamsterModel;
 }
 // Remove the Hamster
 
-const HamsterCard = ({ hamster }: Props) => {
-  async function deleteAHamster(id: string) {
-    await fetch(`/hamsters/${id}`, { method: "DELETE" });
-    window.location.reload();
+
+const HamsterCard =({hamster }: Props) => {
+const [, setData] = useRecoilState<HamsterModel[]>(allHamsters);
+
+	const hamsterCard = async () => {
+const response: Response = await fetch(makeImg(`/hamsters/${hamster.id}`), {
+	method: 'DELETE',
+	headers: {
+	  'Content-Type': 'application/json'
+	},
+	body: null
+  })
+  if (response.status === 200) {
+	
+
+	async function getData() {
+	  const response: Response = await fetch(makeImg('/hamsters/'))
+	  const apiData: any = await response.json()
+
+	  setData(apiData as HamsterModel[])
+	}
+	getData()
   }
 
+}
   return (
     <div className={styles.wrapperGallery}>
         <div className={styles.hamsterInfo}>
@@ -24,7 +46,7 @@ const HamsterCard = ({ hamster }: Props) => {
           <p>
             {hamster.name} loves {hamster.loves} and eating {hamster.favFood}
           </p>
-          <button onClick={() => deleteAHamster(hamster.id)}>
+          <button onClick={() => hamsterCard}>
             Remove Hamster
           </button>
         </div>
@@ -32,3 +54,5 @@ const HamsterCard = ({ hamster }: Props) => {
   );
 };
 export default HamsterCard;
+
+
